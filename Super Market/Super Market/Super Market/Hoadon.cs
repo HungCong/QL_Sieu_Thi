14f48c013758f7b96c3ece8096317c4ae57bb7d7
HungCong <31845368+HungCong@users.noreply.github.com>
2017-10-15 22:47:28 +0700

    Quản lý siêu thị

---
+++ Super Market/Super Market/Super Market/Hoadon.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Super_Market
+{
+    class Cart
+    {
+      
+        public Cart()
+        {
+
+        }
+        public Cart(String productId,String productName,int quantity,decimal productPrice)
+        {
+            
+        }
+        public String _ProductId
+        {
+           
+        }
+        public String _ProductName
+        {
+            
+        }
+        public int _Quantity {
+           
+        }
+        public decimal _ProductPrice
+        {
+            
+        }
+        public decimal _Total
+        {
+            
+            
+        }
+    }
+    class HoaDon {
+        private List<Cart> cart;
+        public HoaDon()
+        { }
+           
+        public List<Cart> _Cart {
+            
+        }
+        //tim mot san pham trong gio? hang`
+       
+        //insert 1 san pham vao
+        public void insert_item_toCart(String productid, String productName, int quantity, decimal productPrice)
+        {
+           
+        }
+        public void remove_item(String Productid)
+        {
+          
+        }
+        public decimal tong_HoaDon
+        {
+                        
+        }
+    }
+   
+}
